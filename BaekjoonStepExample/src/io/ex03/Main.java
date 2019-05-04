2fa2ece30d38dc9a6491006250db1ade9afce5ab
msnodeve <msnodeve@gmail.com>
2019-05-04 15:14:59 +0900

    I/O 3번 문제 성공

--- BaekjoonStepExample/src/io/ex03/Main.java
+++ BaekjoonStepExample/src/io/ex03/Main.java
@@ -1,13 +1,17 @@
-package io.ex02;
+package io.ex03;
 
 import java.util.Scanner;
+
 public class Main {
+
 	public static void main(String[] args) {
 		int A, B;
 		Scanner scan = new Scanner(System.in);
 		A = scan.nextInt();
+		A = 0 < A ? A : 0;
 		B = scan.nextInt();
-		
-		System.out.println(A+B);
+		B = 10 > B ? B : 0;
+		System.out.println(A-B);
 	}
+
 }
