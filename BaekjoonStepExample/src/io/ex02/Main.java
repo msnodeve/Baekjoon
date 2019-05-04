3a000fac106a1df7f668bcaca102eccbb5da5cf7
msnodeve <msnodeve@gmail.com>
2019-05-04 15:09:15 +0900

    I/O 2번 문제 성공

--- BaekjoonStepExample/src/io/ex02/Main.java
+++ BaekjoonStepExample/src/io/ex02/Main.java
@@ -1,7 +1,14 @@
 package io.ex02;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-
+		int A, B;
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextInt();
+		B = scan.nextInt();
+		
+		System.out.println(A+B);
 	}
 }
