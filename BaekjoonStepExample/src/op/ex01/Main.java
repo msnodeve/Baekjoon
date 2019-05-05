bebef91b1a08527be34f8c041403ff4ece2bdb54
msnodeve <msnodeve@gmail.com>
2019-05-05 16:25:54 +0900

    알고리즘 문제 정리

---
+++ BaekjoonStepExample/src/op/ex01/Main.java
@@ -0,0 +1,13 @@
+package op.ex01;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		int A, B;
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextInt();
+		B = scan.nextInt();
+		
+		System.out.println(A+B);
+	}
+}
