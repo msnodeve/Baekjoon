db2ddcfc54196e3f1df1cff8121ead6036942854
msnodeve <msnodeve@gmail.com>
2019-05-05 16:58:07 +0900

    사칙연산 문제 해결

---
+++ BaekjoonStepExample/src/op/ex06/Main.java
@@ -0,0 +1,18 @@
+package op.ex06;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		int A, B, C;
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextInt();
+		A = 2 <= A ? A : 0;
+		B = scan.nextInt();
+		C = scan.nextInt();
+		C = 10000 >= C ? C : 0;
+		System.out.println((A + B) % C);
+		System.out.println((A % C + B % C) % C);
+		System.out.println((A * B) % C);
+		System.out.println((A % C * B % C) % C);
+	}
+}
--- BaekjoonStepExample/src/op/ex07/Main.java
+++ BaekjoonStepExample/src/op/ex07/Main.java
@@ -1,14 +1,14 @@
-package op.ex04;
+package op.ex07;
 
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		double A, B;
+		int A, B;
 		Scanner scan = new Scanner(System.in);
 		A = scan.nextInt();
 		A = 0 < A ? A : 0;
 		B = scan.nextInt();
 		B = 10 > B ? B : 0;
-		System.out.println(A/B);
+		System.out.println(A+B);
 	}
 }
