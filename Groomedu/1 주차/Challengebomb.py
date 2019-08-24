29cbb52257b82e44da09f017bea00a7ba8543c2a
김명석 <seok@gimmyeongseog-ui-MacBook-Pro.local>
2019-08-24 16:14:25 +0900

    교수수님의 과제 폭탄, 1주차 3번째 문제

--- Groomedu/1 주차/Challengebomb.py
+++ Groomedu/1 주차/Challengebomb.py
@@ -19,15 +19,11 @@ for i in range(0, user_input):
 
 # 1번째 인자 오름차순 정리, 2번째 인자 내림차순 정리
 monthList = sorted(monthList, key = lambda x : (x[0], -x[1]))
+# print(monthList)
 stack = []
 flag = True
 compare=monthList[0];
 for i in range(1, len(monthList)):
-	if len(stack) != 0:
-		for j in stack:
-			if j[1] < monthList[i][0]:
-				print("No")
-				sys.exit(0)
 	# 경우 1, 이전 마감보다 새로운 시작이 같거나 크면 통과
 	if compare[1] <= monthList[i][0]:
 		compare = monthList[i]
@@ -37,9 +33,16 @@ for i in range(1, len(monthList)):
 		# 이전 마감과 새로운 마감이 같은 경우
 		if compare[1] == monthList[i][1]:
 			compare = monthList[i]
-		else:
+		# 이전 마감보다 새로운 마감이 작은 경우
+		elif compare[1] > monthList[i][1]:
 			stack.append(compare)
 			compare = monthList[i]
+		else:
+			if len(stack) != 0:
+				for j in stack:
+					if j[1] < monthList[i][1]:
+						print("No")
+						sys.exit(0)
 
 if flag:
 	print("Yes")
