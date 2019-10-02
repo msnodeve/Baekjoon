8193812cea4d3dff0bafc82f41cce449090c3118
msnodeve <msnodeve@gmail.com>
2019-10-02 14:30:24 +0900

    스탈린 정렬

---
+++ Groomedu/비타알골 시즌 1/9 주차/9-1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+import bisect
+import numpy as np
+
+N = int(input())
+arr = [int (i) for i in input().split(" ")]
+
+lst = np.zeros(N)
+l = 0
+for i in range(N):
+	pos = bisect.bisect_left(lst, arr[i], 0, l)
+	lst[pos] = arr[i]
+	if pos == l:
+		l += 1
+
+print(N-l)
