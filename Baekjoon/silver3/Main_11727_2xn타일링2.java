6af73af720c3f6995221fe06b17c174d6b1b2f3e
msnodeve <msnodeve@gmail.com>
2020-04-20 17:42:18 +0900

    동적계획법

---
+++ Baekjoon/silver3/Main_11727_2xn타일링2.java
@@ -0,0 +1,26 @@
+package silver3;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Main_11727_2xn타일링2 {
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/baekjoon/sample_input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력 및 처리
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+        if (n != 1) {
+            for (int i = 3; i < n + 1; i++) {
+                dp[i] = ((dp[i - 1] % 10007) + (2 * (dp[i - 2] % 10007) % 10007)) % 10007;
+            }
+        }
+        System.out.println(dp[n] % 10007);
+    }
+}
